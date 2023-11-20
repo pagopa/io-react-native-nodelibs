61b793bdb1a628bdce6f9465949c2201bae733ff
Francesco Grauso <grausof@gmail.com>
2023-11-20 14:37:18 +0100

    feat: Add `crypto` modules

--- globals.js
+++ globals.js
@@ -1,13 +1,13 @@
-global.Buffer = require('buffer').Buffer;
-global.process = require('process');
+global.Buffer = require("buffer").Buffer;
+global.process = require("process");
 
 // Needed so that 'stream-http' chooses the right default protocol.
 global.location = {
-  protocol: 'file:',
+  protocol: "file:",
 };
 
 // Some modules expect userAgent to be a string
-global.navigator.userAgent = 'React Native';
+global.navigator.userAgent = "React Native";
 
 const { encode, decode } = require("base-64");
 global.atob = decode;
